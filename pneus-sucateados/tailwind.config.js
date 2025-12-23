81e022cdc731d33f0d55328921592517ef2b31bd
TO-BRASIL <vinicius.nogueira@vflows.com.br>
2025-12-23 09:25:01 -0300

    ajuustes

---
+++ pneus-sucateados/tailwind.config.js
@@ -0,0 +1,28 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            colors: {
+                primary: {
+                    50: '#f0f9ff',
+                    100: '#e0f2fe',
+                    500: '#0ea5e9',
+                    600: '#0284c7',
+                    700: '#0369a1',
+                    800: '#075985',
+                    900: '#0c4a6e',
+                },
+                'vermelho': '#C1272D',
+                'azul-01': '#43479A',
+                'azul-02': '#031590',
+                'azul-03': '#2D44BD',
+                'azul-04': '#1226A2',
+                'preto-90': '#1A1A1A',
+                'preto-70': '#4D4D4D',
+                'azul-05': '#6699FF',
+            }
+        }
+    }
+}
